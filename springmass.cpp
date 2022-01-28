0f459fc4aae4d84ae8ceb83e023ae3e46472870d
jiahao-0204 <75384852+jiahao-0204@users.noreply.github.com>
2022-01-28 12:46:31 +0000

    add graphic support for springmass

--- springmass.cpp
+++ springmass.cpp
@@ -11,81 +11,76 @@
 // class Mass
 /* ---------------------------------------------------------------- */
 
-Mass::Mass()
-: position(), velocity(), force(), mass(1), radius(1)
-{ }
+Mass::Mass() : position(), velocity(), force(), mass(1), radius(1) {}
 
-Mass::Mass(Vector2 position, Vector2 velocity, double mass, double radius)
-: position(position), velocity(velocity), force(), mass(mass), radius(radius),
-xmin(-1),xmax(1),ymin(-1),ymax(1)
-{ }
+Mass::Mass(Vector2 position, Vector2 velocity, double mass, double radius) 
+: position(position), velocity(velocity), force(), mass(mass), radius(radius), xmin(-1),xmax(1),ymin(-1),ymax(1) {}
 
-void Mass::setForce(Vector2 f)
-{
+
+void Mass::setForce(Vector2 f) {
   force = f ;
 }
 
-void Mass::addForce(Vector2 f)
-{
+void Mass::addForce(Vector2 f) {
   force = force + f ;
 }
 
-Vector2 Mass::getForce() const
-{
+Vector2 Mass::getForce() const {
   return force ;
 }
 
-Vector2 Mass::getPosition() const
-{
+Vector2 Mass::getPosition() const {
   return position ;
 }
 
-Vector2 Mass::getVelocity() const
-{
+Vector2 Mass::getVelocity() const {
   return velocity ;
 }
 
-double Mass::getRadius() const
-{
+double Mass::getRadius() const {
   return radius ;
 }
 
-double Mass::getMass() const
-{
+double Mass::getMass() const {
   return mass ;
 }
 
-double Mass::getEnergy(double gravity) const
-{
-  double height = position.y ;  
-  return mass * gravity * height ;
+double Mass::getEnergy(double gravity) const {
+  double energy = 0;
+
+  // potential energy
+  double height = position.y - ymin;  
+  energy += mass * gravity * height ;
+
+  // kinetic energy 
+  energy += 1/2 * mass * velocity.norm2();
+
+  return energy;
+
 }
 
-void Mass::step(double dt)
-{
+void Mass::step(double dt) {
   // new position and velocity
-  Vector2 position_pending = position + velocity * dt + 1/2 * force / mass * dt * dt; 
+  Vector2 position_pending = position + velocity * dt; //  + 1/2 * force / mass * dt * dt 
   Vector2 velocity_pending = velocity + force / mass * dt; 
 
   // x direction
-  if (xmin <= position_pending.x - radius && position_pending.x + radius <= xmax)
-  {
+  if (xmin <= position_pending.x - radius && position_pending.x + radius <= xmax) {
+
     position.x = position_pending.x;
     velocity.x = velocity_pending.x;
-  }
-  else
-  {
+
+  } else {
+
     velocity.x = - velocity.x;
+
   }
 
   // y direction
-  if (ymin <= position_pending.y - radius && position_pending.y + radius <= ymax)
-  {
+  if (ymin <= position_pending.y - radius && position_pending.y + radius <= ymax) {
     position.y = position_pending.y;
     velocity.y = velocity_pending.y;
-  }
-  else
-  {
+  } else {
     velocity.y = - velocity.y;
   }
 
@@ -97,21 +92,17 @@ void Mass::step(double dt)
 
 Spring::Spring(Mass * mass1, Mass * mass2, double naturalLength, double stiffness, double damping)
 : mass1(mass1), mass2(mass2),
-naturalLength(naturalLength), stiffness(stiffness), damping(damping)
-{ }
+naturalLength(naturalLength), stiffness(stiffness), damping(damping) {}
 
-Mass * Spring::getMass1() const
-{
+Mass * Spring::getMass1() const {
   return mass1 ;
 }
 
-Mass * Spring::getMass2() const
-{
+Mass * Spring::getMass2() const {
   return mass2 ;
 }
 
-Vector2 Spring::getForce() const
-{
+Vector2 Spring::getForce() const {
   // mass information
   Vector2 x1 = mass1 -> getPosition();
   Vector2 x2 = mass2 -> getPosition();
@@ -131,11 +122,15 @@ Vector2 Spring::getForce() const
   return F ;
 }
 
-double Spring::getLength() const
-{
+double Spring::getLength() const {
   Vector2 u = mass2->getPosition() - mass1->getPosition() ;
   return u.norm() ;
 }
+
+double Spring::getStiffness() const {
+  return stiffness;
+}
+
 
 double Spring::getEnergy() const {
   double length = getLength() ;
@@ -143,34 +138,44 @@ double Spring::getEnergy() const {
   return 0.5 * stiffness * dl * dl ;
 }
 
-std::ostream& operator << (std::ostream& os, const Mass& m)
-{
+
+// just saw this, could have been useful when display results
+std::ostream& operator << (std::ostream& os, const Mass& m) {
   os<<"("
   <<m.getPosition().x<<","
   <<m.getPosition().y<<")" ;
   return os ;
 }
 
-std::ostream& operator << (std::ostream& os, const Spring& s)
-{
+std::ostream& operator << (std::ostream& os, const Spring& s) {
   return os<<"$"<<s.getLength() ;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* ---------------------------------------------------------------- */
 // class SpringMass : public Simulation
 /* ---------------------------------------------------------------- */
 
-SpringMass::SpringMass()
-{ 
-  gravity = MOON_GRAVITY;
+SpringMass::SpringMass() { 
+  gravity = EARTH_GRAVITY;
 }
 
 
 
-void SpringMass::addSpring(std::vector<Spring> more_springs)
-{
-  for (std::vector<Spring>::iterator it = begin(more_springs); it != end (more_springs); ++it) 
-  {
+void SpringMass::addSpring(std::vector<Spring> more_springs) {
+  for (std::vector<Spring>::iterator it = begin(more_springs); it != end (more_springs); ++it) {
     // add springs
     spring_list.push_back(*it) ; 
 
@@ -179,34 +184,49 @@ void SpringMass::addSpring(std::vector<Spring> more_springs)
   }
 }
 
-void SpringMass::addMass(Spring _spring)
-{
+void SpringMass::addMass(Spring _spring) {
   Mass * mass1 = _spring.getMass1();
   Mass * mass2 = _spring.getMass2();
 
   // append if not present
-  if ( std::find(mass_list.begin(), mass_list.end(), mass1) == mass_list.end() )
-  {
+  if ( std::find(mass_list.begin(), mass_list.end(), mass1) == mass_list.end() ) {
     mass_list.push_back(mass1);
   }
-  if ( std::find(mass_list.begin(), mass_list.end(), mass2) == mass_list.end() )
-  {
+  if ( std::find(mass_list.begin(), mass_list.end(), mass2) == mass_list.end() ) {
     mass_list.push_back(mass2);
   }
 
 
 }
 
-void SpringMass::setGravity(double _gravity)
-{
+
+void SpringMass::loadSample() {
+  // mass
+  const double mass = 1 ;
+  const double radius = 0.1 ;
+  Mass * m1 = new Mass(Vector2(-0.5,0), Vector2(0, 0), mass, radius) ;
+  Mass * m2 = new Mass(Vector2(+0.5,0), Vector2(0, 0), mass, radius) ; // need destructor
+  
+  // spring
+  const double naturalLength = 0.5;
+  const double stiff = 0;
+  const double damping = 0;
+  Spring spring1(m1, m2, naturalLength, stiff, damping) ;
+
+  // spring vector
+  std::vector<Spring> more_springs(1, spring1);
+
+  // springmass
+  addSpring(more_springs);
+}
+
+void SpringMass::setGravity(double _gravity) {
   gravity = _gravity;
 }
 
-void SpringMass::display()
-{
+void SpringMass::display() {
   // multiple mass per line
-  for (std::vector<Mass *>::iterator it = begin(mass_list); it != end (mass_list); ++it)
-  {
+  for (std::vector<Mass *>::iterator it = begin(mass_list); it != end (mass_list); ++it) {
     Vector2 position = (*it) -> getPosition();
     std::cout << position.x << " " << position.y << " ";
   } 
@@ -214,29 +234,33 @@ void SpringMass::display()
   std::cout << std::endl;
 }
 
-double SpringMass::getEnergy() const
-{
+double SpringMass::getEnergy() {
   double energy = 0 ;
+  
+  // mass
+  for (std::vector<Mass *>::iterator it = mass_list.begin(); it != mass_list.end(); ++it) {
+    energy += (*it)->getEnergy(gravity);
+  }
 
-/* INCOMPLETE: TYPE YOUR CODE HERE */
-
+  // spring
+  for (std::vector<Spring>::iterator it = spring_list.begin(); it != spring_list.end(); ++it) {
+    energy += (*it).getEnergy();
+  }
+  
   return energy ;
 }
 
-void SpringMass::step(double dt)
-{
+void SpringMass::step(double dt) {
 
   // set initial force 
   Vector2 g(0, -gravity) ;
-  for (std::vector<Mass *>::iterator it = begin(mass_list); it != end (mass_list); ++it)
-  {
+  for (std::vector<Mass *>::iterator it = mass_list.begin(); it != mass_list.end(); ++it) {
     (*it) -> setForce(g * (*it)->getMass());
   } 
   
 
   // get spring force and add force
-  for (std::vector<Spring>::iterator it = begin(spring_list); it != end (spring_list); ++it)
-  {
+  for (std::vector<Spring>::iterator it = spring_list.begin(); it != spring_list.end(); ++it) {
     // get mass
     Mass * mass1 = (*it).getMass1();
     Mass * mass2 = (*it).getMass2();
@@ -249,11 +273,10 @@ void SpringMass::step(double dt)
     mass1 -> addForce(F1);
     mass2 -> addForce(F2);
   }
-  
+
 
   // update
-  for (std::vector<Mass *>::iterator it = begin(mass_list); it != end (mass_list); ++it)
-  {
+  for (std::vector<Mass *>::iterator it = mass_list.begin(); it != mass_list.end(); ++it) {
     (*it) -> step(dt);
   } 
   
--- test-springmass-graphics.cpp
+++ test-springmass-graphics.cpp
@@ -11,22 +11,79 @@
 #include <iomanip>
 
 /* ---------------------------------------------------------------- */
-class SpringMassDrawable : public SpringMass, public Drawable
+class SpringMassDrawable : public SpringMass, public Drawable {
 /* ---------------------------------------------------------------- */
-{
 
-/* INCOMPLETE: TYPE YOUR CODE HERE */
+  private:
+    Figure figure ;
+
+  public:
+    SpringMassDrawable() : figure("Spring Mass") {
+      figure.addDrawable(this) ;
+    }
+
+    void draw() {
+
+      // draw mass
+      for (std::vector<Mass *>::iterator it = begin(mass_list); it != end (mass_list); ++it) {
+        // position and radius
+        Vector2 position = (*it)->getPosition();
+        double r = (*it)->getRadius();
+        
+        // draw
+        figure.drawCircle(position.x, position.y, r) ;
+      }
+      
+      // draw spring
+      for (std::vector<Spring>::iterator it = begin(spring_list); it != end (spring_list); ++it) {
+        // position
+        Mass * m1 = (*it).getMass1();
+        Mass * m2 = (*it).getMass2();
+
+        Vector2 p1 = m1->getPosition();
+        Vector2 p2 = m2->getPosition();
+
+        // thickness
+        double thickness = (*it).getStiffness();
+        
+        // draw
+        figure.drawLine(p1.x, p1.y, p2.x, p2.y, thickness) ;
+      }
+
+
+      // draw energy
+
+      // x and y
+      double x = 0;
+      double y = 0;
+
+      // energy  
+      double energy = getEnergy();
+      std::stringstream ss;
+      ss << energy;
+      std::string energy_str = ss.str();
+
+      figure.drawString(x, y, energy_str);
+
+    }
+
+    void display() {
+      figure.update() ;
+    }
 
 } ;
 
-int main(int argc, char** argv)
-{
-  glutInit(&argc,argv) ;
+int main(int argc, char** argv) {
+  // GLUT
+  glutInit(&argc,argv);
 
-  SpringMassDrawable springmass ;
+  // run simulation
+  SpringMassDrawable springmass;
 
+  springmass.loadSample();
 
-/* INCOMPLETE: TYPE YOUR CODE HERE */
+  run(&springmass, 1/120.0);
 
-  run(&springmass, 1/120.0) ;
+  // return 
+  return 0 ;
 }
