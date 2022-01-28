8e8b0ab03654cbbf557f68ef3a8b7796aa37f6c7
jiahao-0204 <75384852+jiahao-0204@users.noreply.github.com>
2022-01-28 14:06:26 +0000

    test multi-mass, success

--- test-springmass-graphics.cpp
+++ test-springmass-graphics.cpp
@@ -51,11 +51,9 @@ class SpringMassDrawable : public SpringMass, public Drawable {
 
 
       // draw energy
-
       // x and y
       double x = 0;
       double y = 0;
-
       // energy  
       double energy = getEnergy();
       std::stringstream ss;
@@ -80,8 +78,31 @@ int main(int argc, char** argv) {
   // run simulation
   SpringMassDrawable springmass;
 
-  springmass.loadSample();
+
+  // springmass.loadSample();
+  // mass
+  const double mass = 1 ;
+  const double radius = 0.1 ;
+  Mass * m1 = new Mass(Vector2(-0.5,0), Vector2(0, 0), mass, radius) ;
+  Mass * m2 = new Mass(Vector2(+0.5,0), Vector2(1, 2), mass, radius) ; // need destructor
+  Mass * m3 = new Mass(Vector2(+0.5,0.5), Vector2(0, 0), mass, radius) ; // need destructor
   
+  // spring
+  const double naturalLength = 0.5;
+  const double stiff = 1;
+  const double damping = 1;
+  Spring spring1(m1, m2, naturalLength, 0, 0) ;
+  Spring spring2(m2, m3, naturalLength, 1, 0) ;
+  Spring spring3(m3, m1, naturalLength, 1, 0) ;
+
+  // spring vector
+  std::vector<Spring> more_springs(1, spring1);
+  more_springs.push_back(spring2);
+  more_springs.push_back(spring3);
+
+  // springmass
+  springmass.addSpring(more_springs);
+
   run(&springmass, 1.0/240.0);
 
   // return 
